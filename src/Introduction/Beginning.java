129bde649ed2e706943bc507ba4483db097531b3
Buse Karakaş <47915824+bskrks@users.noreply.github.com>
2023-11-28 15:49:18 +0300

    Add  input example

--- src/Introduction/Beginning.java
+++
@@ -1,7 +0,0 @@
-package Introduction;
-
-public class Beginning {
-    public static void main(String[] args) {
-        System.out.print("Main method and creating a new project");
-    }
-}
---
+++ src/Main.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        String name;
+        int age, salary;
+
+        System.out.print("Enter your name : ");
+        name = input.nextLine();
+
+        System.out.print("Enter your age : ");
+        age = input.nextInt();
+
+        System.out.print("Enter your salary : ");
+        salary = input.nextInt();
+
+        System.out.print("Name : "+ name + "\nAge : "+ age +"\nSalary : "+ salary);
+
+    }
+}
